ba7a85c593e60414f86727d3d084f0b5ba4aba49
Dominik Wei-Fieg <dominik@localhost.(none)>
2011-02-17 09:18:05 +0100

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,41 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "photocaching"
+set :repository,  "git@github.com:dominikweifieg/Photocaching.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :use_sudo, true
+
+set :deploy_to, "ls /opt/bitnami/apps/#{application}"
+
+set :user, "bitnami"
+
+ssh_options[:forward_agent] = true
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+set :main_server, "ec2-46-137-9-5.eu-west-1.compute.amazonaws.com"
 
 role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 role :app, "your app-server here"                          # This may be the same as your `Web` server
 role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+#role :db,  "your slave db-server here"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    #do nothing
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
