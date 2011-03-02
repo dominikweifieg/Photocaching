1f6e310bce5e51d96ee0623973ead829459f342a
Dominik Wei-Fieg <dominik@localhost.(none)>
2011-03-02 10:51:44 +0100

    woop

--- db/migrate/20110302091338_update_plays_table.rb
+++ db/migrate/20110302091338_update_plays_table.rb
@@ -1,11 +1,9 @@
 class UpdatePlaysTable < ActiveRecord::Migration
   def self.up
-    add_column :plays, :url, :string
     add_column :plays, :thumb, :string
   end
 
   def self.down
     remove_column :plays, :thumb
-    remove_column :plays, :url
   end
 end
