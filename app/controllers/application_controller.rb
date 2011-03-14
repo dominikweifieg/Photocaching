dedcce41303d012e402719676d39a246f5bc6470
Dominik Wei-Fieg <dominik@localhost.(none)>
2011-03-14 10:57:32 +0100

    started mobile modifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
+  layout :detect_browser
+
+
   def authenticate
     authenticate_or_request_with_http_digest("Photocaching Access") do |user_name|
-        case request.format
-        when Mime::XML, Mime::ATOM, Mime::JSON
-          if user_name == "fact2]yuccas"
-            return "cUK4BH_AFfWA}#Sfk{?X1Bh)e" 
-          else
-            return ""
-          end
+      case request.format
+      when Mime::XML, Mime::ATOM, Mime::JSON
+        if user_name == "fact2]yuccas"
+          return "cUK4BH_AFfWA}#Sfk{?X1Bh)e" 
         else
-          user = User.find_by_alias(user_name)
-          user.identifier.hash.to_s(36)
+          return ""
         end
+      else
+        user = User.find_by_alias(user_name)
+        user.identifier.hash.to_s(36)
       end
+    end
   end
-  
+
   def location_for_latitude(lat)
     bucket = "photocaching.test" 
     server = "s3"
-    
+
     logger.info(lat)
-    
+
     if(lat >= -180.0 && lat < -100.0)
       logger.info("us west")
       bucket = "photocaching.us.west"
@@ -44,7 +47,29 @@ class ApplicationController < ActionController::Base
       bucket = "photocaching.asia.east"
       server = "s3-ap-northeast-1"
     end
-    
+
     return "http://#{server}.amazonaws.com/#{bucket}"
   end
+
+  private
+  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
+  def select_layout
+    session.inspect # force session load
+    if session.has_key? "layout"
+      return (session["layout"] == "mobile") ? "mobile_application" : "application"
+    end
+    return detect_browser
+  end
+
+  def detect_browser
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      session["layout"] = "mobile"
+      return "mobile_application" if agent.match(m)
+    end
+    session["layout"] = "foo"
+    return "application"
+  end
+
 end
