8fff4192141fdc98da31483365db8866b0735cb4
Dominik Wei-Fieg <dominik@localhost.(none)>
2011-04-30 11:16:31 +0200

    added second server

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,10 @@ set :branch, "master"
 #set :deploy_via, :remote_cache
 
 set :main_server, "ec2-46-137-9-5.eu-west-1.compute.amazonaws.com"
+set :second_server, "ec2-79-125-59-200.eu-west-1.compute.amazonaws.com"
 
-role :web, main_server                          # Your HTTP server, Apache/etc
-role :app, main_server                          # This may be the same as your `Web` server
+role :web, main_server, second_server                          # Your HTTP server, Apache/etc
+role :app, main_server, second_server                          # This may be the same as your `Web` server
 role :db,  main_server, :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
